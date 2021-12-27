96ab192af5078b0040809b5f4dd2cbddb77c82d3
Utsav Karan <ukaran11@gmail.com>
2021-12-28 01:46:45 +0530

    Utsav | Completed setup app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "babel-polyfill";
+import React from "react";
+import { render } from "react-dom";
+import { createGlobalStyle } from "styled-components";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import SignUp from "./components/SignUp";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,600&display=swap');
+
+  body {
+    font-family: Roboto, sans-serif;
+  }
+`;
+
+render(
+  <>
+    <SignUp />
+    <GlobalStyle />
+  </>,
+  document.getElementById("app")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
